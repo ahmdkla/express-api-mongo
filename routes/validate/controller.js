d5f010d289d06b1a44fbe2625ef38c5a2137b27b
Ahmad Rafiqul A'la <ahmadakla48@gmail.com>
2019-12-06 00:07:37 +0700

    change something

--- routes/validate/controller.js
+++ routes/validate/controller.js
@@ -1,30 +1,28 @@
 const signup = require("./signup");
-const signin = require("./signin")
+const signin = require("./signin");
 
 module.exports = () => {
-    const validate = async (req, res) => {
-      
-  
-        try {
-            const {
-                body,
-                params: { id }
-            } = req;
-            let result = {};
+  const validate = async (req, res) => {
+    try {
+      const {
+        body,
+        params: { id }
+      } = req;
+      let result = {};
 
-            switch (id) {
-                case "signup":
-                    return res.json(await signup(body));
-                case "signin":
-                    return res.json(await signin(body));
-                default:
-                    return res.json(result);
-            }
-        } catch (error) {
-            res.status(400).json(error);
-        }
-    };
-    return {
-        validate
-    };
+      switch (id) {
+        case "signup":
+          return res.json(await signup(body));
+        case "signin":
+          return res.json(await signin(body));
+        default:
+          return res.json(result);
+      }
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  };
+  return {
+    validate
+  };
 };
