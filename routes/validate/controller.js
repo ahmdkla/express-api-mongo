02ea937d8d2ed68c54291204e8149542282cd3c7
Ahmad Rafiqul A'la <33902875+ahmdkla@users.noreply.github.com>
2019-12-06 00:04:23 +0700

    Update controller.js

--- routes/validate/controller.js
+++ routes/validate/controller.js
@@ -3,26 +3,7 @@ const signin = require("./signin")
 
 module.exports = () => {
     const validate = async (req, res) => {
-        
-        try {
-            const {
-                body,
-                params: { id }
-            } = req;
-            let result = {};
-
-            switch (id) {
-                case "signup":
-                    return res.json(await signup(body));
-                case "signin":
-                    return res.json(await signin(body));
-                default:
-                    return res.json(result);
-            }
-        } catch (error) {
-            res.status(400).json(error);
-        }
-    };
+      
 
     return {
         validate
